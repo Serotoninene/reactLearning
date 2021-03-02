f5dddcdf127acc1928d424e2ab9b025ec1bc04b7
Serotoninene <pujol.alexandre@hotmail.fr>
2021-03-02 10:54:31 +0100

    starting the dadjokes app

--- deck/src/Card.js
+++ deck/src/Card.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
 class Card extends Component {
-
+    
     render(){
         let cards_img = this.props.cards_img
-        let random_rotation = Math.floor(Math.random() * 10)  
-        let style = "transform : translate(-50%, -50%)"
+        let random_rotation = Math.floor(Math.random() * 90)  
+        let style = `rotate(${random_rotation}deg)`
         let card = cards_img.map((e, idx) => {
-            return <img src = {e} alt = 'card' key = {idx}/> 
+            return <img src = {e} alt = 'card' key = {idx} style = {{transform : style}}/> 
         })
         
 
